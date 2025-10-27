b01c8d6f136431a95bc8bba1019a0ed3f850acb5
Shayne Sivley <shayne@fennon.com>
2025-10-26 18:01:22 -0600

    SSL Cert Layout Change

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1008,56 +1008,58 @@ function createSslCard(cert) {
     const validTo = new Date(cert.validTo).toLocaleDateString();
     const lastChecked = new Date(cert.lastChecked).toLocaleString();
 
+    const daysDisplay = cert.daysUntilExpiration < 0
+        ? `<div class="days-number">EXPIRED</div>`
+        : `<div class="days-number">${cert.daysUntilExpiration}</div><div class="days-label">days left</div>`;
+
     div.innerHTML = `
-        <div class="ssl-header">
-            <div>
-                <div class="ssl-title">${escapeHtml(cert.friendlyName)}</div>
-                <div class="ssl-url">${escapeHtml(cert.url)}</div>
-            </div>
-            <div class="ssl-actions">
-                <button class="icon-btn" onclick="checkSslNow(${cert.id})" title="Check Now">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                    </svg>
-                </button>
-                <button class="icon-btn" onclick="openSslModal(${cert.id})" title="Edit">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                    </svg>
-                </button>
-                <button class="icon-btn delete" onclick="deleteSslCertificate(${cert.id})" title="Delete">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-
         <div class="days-badge ${statusClass}">
-            ${cert.daysUntilExpiration < 0 ? 'Expired' : `${cert.daysUntilExpiration} days remaining`}
+            ${daysDisplay}
         </div>
 
-        <div class="ssl-info">
-            <div class="ssl-info-row">
-                <span class="ssl-label">Status</span>
-                <span class="ssl-status ${statusClass}">${statusText}</span>
+        <div class="ssl-header">
+            <div class="ssl-title">${escapeHtml(cert.friendlyName)}</div>
+            <div class="ssl-url">${escapeHtml(cert.url)}</div>
+            <div class="ssl-info">
+                <div class="ssl-info-item">
+                    <span class="ssl-label">Status</span>
+                    <span class="ssl-status ${statusClass}">${statusText}</span>
+                </div>
+                <div class="ssl-info-item">
+                    <span class="ssl-label">Valid From</span>
+                    <span class="ssl-value">${validFrom}</span>
+                </div>
+                <div class="ssl-info-item">
+                    <span class="ssl-label">Valid To</span>
+                    <span class="ssl-value">${validTo}</span>
+                </div>
+                <div class="ssl-info-item">
+                    <span class="ssl-label">Issuer</span>
+                    <span class="ssl-value">${escapeHtml(cert.issuer.split(',')[0])}</span>
+                </div>
+                <div class="ssl-info-item">
+                    <span class="ssl-label">Last Checked</span>
+                    <span class="ssl-value">${lastChecked}</span>
+                </div>
             </div>
-            <div class="ssl-info-row">
-                <span class="ssl-label">Valid From</span>
-                <span class="ssl-value">${validFrom}</span>
-            </div>
-            <div class="ssl-info-row">
-                <span class="ssl-label">Valid To</span>
-                <span class="ssl-value">${validTo}</span>
-            </div>
-            <div class="ssl-info-row">
-                <span class="ssl-label">Issuer</span>
-                <span class="ssl-value" style="font-size: 0.8rem;">${escapeHtml(cert.issuer.split(',')[0])}</span>
-            </div>
-            <div class="ssl-info-row">
-                <span class="ssl-label">Last Checked</span>
-                <span class="ssl-value" style="font-size: 0.8rem;">${lastChecked}</span>
-            </div>
+        </div>
+
+        <div class="ssl-actions">
+            <button class="icon-btn" onclick="checkSslNow(${cert.id})" title="Check Now">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+            </button>
+            <button class="icon-btn" onclick="openSslModal(${cert.id})" title="Edit">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                </svg>
+            </button>
+            <button class="icon-btn delete" onclick="deleteSslCertificate(${cert.id})" title="Delete">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+            </button>
         </div>
     `;
 
